9c279d987758911edcf028ffe1e85e3fc4e8bd1e
cyberw0lves <cyberwolves.contact@gmail.com>
2025-05-19 07:59:38 +0530

    made changes on frontend redirect dashboard page

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,13 +1,12 @@
-import { cookies } from "next/headers";
-
 import Profile from "@/components/Profile";
-
-const verifyIfUserAuthenticated = async () => {
-  const cookieStore = await cookies();
-  const refreshToken = cookieStore.get("refreshToken");
-};
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
+  const cookieStore = cookies();
+  const tokenCookie = (await cookieStore).get("refreshToken")?.value;
+  if (!tokenCookie) redirect("/");
+
   return (
     <div className="w-screen h-screen flex items-center justify-center relative">
       <Profile />
