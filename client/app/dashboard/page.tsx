fe916732f3e135ce1a691d1f6f9b38103cc02ebe
cyberw0lves <cyberwolves.contact@gmail.com>
2025-05-19 08:30:14 +0530

    added loading animation on dashboard page

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,14 +1,56 @@
-import Profile from "@/components/Profile";
-import { getCookie } from "cookies-next";
-import { redirect } from "next/navigation";
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next/client";
 
-const Dashboard = async () => {
-  const tokenCookie = getCookie("refreshToken");
-  if (!tokenCookie) redirect("/");
+import Profile from "@/components/Profile";
+
+const Dashboard = () => {
+  const [load, setLoad] = useState<boolean>(true);
+  const text = "Loading...";
+  const router = useRouter();
+
+  useEffect(() => {
+    const tokenCookie = getCookie("refreshToken");
+    if (!tokenCookie) {
+      router.push("/");
+    } else {
+      setLoad(false);
+    }
+  }, []);
 
   return (
     <div className="w-screen h-screen flex items-center justify-center relative">
-      <Profile />
+      {!load ? (
+        <Profile />
+      ) : (
+        <div className="w-full h-screen flex items-center justify-center">
+          <style>{`
+        @keyframes bounce {
+          0%, 80%, 100% { transform: translateY(0px); }
+          30% { transform: translateY(-30px); }
+          60% { transform: translateY(10px); }
+        }
+      `}</style>
+          <div>
+            {text.split("").map((char, index) => (
+              <span
+                key={index}
+                className="inline-block text-[3rem] text-white"
+                style={{
+                  animation: "bounce 1s ease-in-out infinite",
+                  animationDelay: `${(index + 1) * 0.1}s`,
+                  transformOrigin: "bottom",
+                  fontFamily: '"Sour Gummy", sans-serif',
+                  fontOpticalSizing: "auto",
+                }}
+              >
+                {char}
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
