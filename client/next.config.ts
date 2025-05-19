0a6ee75f26d0eb309dcf259d4833f02ad30cfe17
cyberw0lves <cyberwolves.contact@gmail.com>
2025-05-19 12:07:04 +0530

    added proxy at root of client

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,17 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
